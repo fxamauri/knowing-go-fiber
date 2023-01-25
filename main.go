28f110ea1a37cb8c928a1355b76eb8c7539772da
Amauri Dias <athlon.amauri@gmail.com>
2023-01-25 11:45:59 -0300

    feat: route middleware example

--- main.go
+++ main.go
@@ -10,7 +10,19 @@ func main() {
 	app := fiber.New()
 
 	groupV1 := app.Group("/v1")
+
+	groupV1.Get("health", func(c *fiber.Ctx) error {
+
+		return c.SendString("OK")
+	})
+
 	groupUser := groupV1.Group("/user")
+
+	groupUser.Use(func(c *fiber.Ctx) error {
+		c.Set("My-Custom-Header", "CUSTOM_HEADER_VALUE")
+		c.Next()
+		return nil
+	})
 
 	groupUser.Get("/:name", func(c *fiber.Ctx) error {
 		fmt.Fprintf(c, "hello %s", c.Params("name"))
