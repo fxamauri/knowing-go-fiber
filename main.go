82d2baec348d2310412319ec18f7749233619778
Amauri Dias <athlon.amauri@gmail.com>
2023-01-25 11:36:18 -0300

    feat: route grouping example

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello")
+	groupV1 := app.Group("/v1")
+	groupUser := groupV1.Group("/user")
+
+	groupUser.Get("/:name", func(c *fiber.Ctx) error {
+		fmt.Fprintf(c, "hello %s", c.Params("name"))
+		return nil
 	})
 
 	app.Listen(":3000")
